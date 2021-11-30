a4e27ba264c26269637183df3359de91626b38bd
pserdax <parahatgeldiyew.s@gmail.com>
2021-11-30 17:33:43 +0200

    Implemented Run entity and Run DAO

---
+++ app/src/main/java/com/lyvetech/runorrun/db/RunDAO.kt
@@ -0,0 +1,40 @@
+package com.lyvetech.runorrun.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // When we want to insert a run that already exist in our database it just replaces with new on
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurnt DESC")
+    fun getAllRunsSortedCaloriesBurnt(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY averageSpeed DESC")
+    fun getAllRunsSortedByAverageSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurnt) FROM running_table")
+    fun getTotalCaloriesBurnt(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(averageSpeed) FROM running_table")
+    fun getTotalAverageSpeed(): LiveData<Float>
+}
