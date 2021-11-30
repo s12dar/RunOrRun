4458af1a5a1c97e6434077fa74a0569faf840766
pserdax <parahatgeldiyew.s@gmail.com>
2021-11-30 17:54:57 +0200

    Implementations of Running database and Type converter classes

---
+++ app/src/main/java/com/lyvetech/runorrun/db/RunningDatabase.kt
@@ -0,0 +1,13 @@
+package com.lyvetech.runorrun.db
+
+import androidx.room.Database
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase {
+    abstract fun getRunDao(): RunDAO
+}
