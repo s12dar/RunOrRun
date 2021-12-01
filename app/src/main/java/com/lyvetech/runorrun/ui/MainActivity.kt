40d240051fdc4fbce86ed0f0ac459db7ae7235ff
pserdax <parahatgeldiyew.s@gmail.com>
2021-12-01 14:50:52 +0200

    Bottom navigation implementation

--- app/src/main/java/com/lyvetech/runorrun/ui/MainActivity.kt
+++ app/src/main/java/com/lyvetech/runorrun/ui/MainActivity.kt
@@ -3,8 +3,12 @@ package com.lyvetech.runorrun.ui
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.lyvetech.runorrun.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/com/lyvetech/runorrun/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/lyvetech/runorrun/ui/fragments/SetupFragment.kt
@@ -1,16 +1,19 @@
 package com.lyvetech.runorrun.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.lyvetech.runorrun.R
+import com.lyvetech.runorrun.databinding.FragmentSetupBinding
+import timber.log.Timber
 
 class SetupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentSetupBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +22,13 @@ class SetupFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setup, container, false)
+        binding = FragmentSetupBinding.inflate(inflater, container, false)
+
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+        return binding.root
     }
 }
