5d9eb4a0d7228aeff81728050c7549caa13aba29
pserdax <parahatgeldiyew.s@gmail.com>
2021-12-01 17:29:58 +0200

    Handled location permissions

--- app/src/main/java/com/lyvetech/runorrun/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/lyvetech/runorrun/ui/fragments/RunFragment.kt
@@ -1,19 +1,26 @@
 package com.lyvetech.runorrun.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.lyvetech.runorrun.R
+ import com.lyvetech.runorrun.databinding.FragmentRunBinding
 import com.lyvetech.runorrun.ui.viewmodels.MainViewModel
+import com.lyvetech.runorrun.utils.Constants.Companion.REQUEST_LOCATION_PERMISSION
+import com.lyvetech.runorrun.utils.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment() {
+class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentRunBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +29,57 @@ class RunFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_run, container, false)
+        binding = FragmentRunBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requestLocationPermissions()
+    }
+
+    private fun requestLocationPermissions() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app",
+                REQUEST_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app",
+                REQUEST_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestLocationPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
--- app/src/main/java/com/lyvetech/runorrun/utils/Constants.kt
+++ app/src/main/java/com/lyvetech/runorrun/utils/Constants.kt
@@ -2,6 +2,15 @@ package com.lyvetech.runorrun.utils
 
 class Constants {
     companion object {
+
+        /**
+         * DATABASE
+         */
         const val RUNNING_DATABASE_NAME = "running_db"
+
+        /**
+         * FRAGMENTS
+         */
+        const val REQUEST_LOCATION_PERMISSION = 0
     }
 }
