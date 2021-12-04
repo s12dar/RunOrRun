f95eb4a3f33e107dce9ae43114d9fcc010aa4c0a
pserdax <parahatgeldiyew.s@gmail.com>
2021-12-04 14:25:58 +0200

    Implemented tracking user location in background

--- app/src/main/java/com/lyvetech/runorrun/services/TrackingService.kt
+++ app/src/main/java/com/lyvetech/runorrun/services/TrackingService.kt
@@ -8,25 +8,61 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.maps.model.LatLng
 import com.lyvetech.runorrun.R
 import com.lyvetech.runorrun.ui.MainActivity
-import com.lyvetech.runorrun.ui.fragments.TrackingFragment
 import com.lyvetech.runorrun.utils.Constants.Companion.ACTION_PAUSE_SERVICE
 import com.lyvetech.runorrun.utils.Constants.Companion.ACTION_SHOW_TRACKING_FRAGMENT
 import com.lyvetech.runorrun.utils.Constants.Companion.ACTION_START_OR_RESUME_SERVICE
 import com.lyvetech.runorrun.utils.Constants.Companion.ACTION_STOP_SERVICE
+import com.lyvetech.runorrun.utils.Constants.Companion.FASTEST_LOCATION_INTERVAL
+import com.lyvetech.runorrun.utils.Constants.Companion.LOCATION_UPDATE_INTERVAL
 import com.lyvetech.runorrun.utils.Constants.Companion.NOTIFICATION_CHANNEL_ID
 import com.lyvetech.runorrun.utils.Constants.Companion.NOTIFICATION_CHANNEL_NAME
 import com.lyvetech.runorrun.utils.Constants.Companion.NOTIFICATION_ID
+import com.lyvetech.runorrun.utils.TrackingUtility
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
     private var TAG = TrackingService::class.qualifiedName
     private var isFirstRun = true
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValuesOfPolylines() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    @SuppressLint("VisibleForTests")
+    override fun onCreate() {
+        super.onCreate()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -52,6 +88,57 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    private val locationCallback by lazy {
+        object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult?) {
+                super.onLocationResult(result)
+                if (isTracking.value!!) {
+                    result!!.locations.let {
+                        for (location in it) {
+                            addPathPoint(location)
+                            Log.d(TAG, "New location: ${location.latitude}, ${location.latitude}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val position = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(position)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     @SuppressLint("UnspecifiedImmutableFlag")
     private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
         this,
@@ -63,6 +150,9 @@ class TrackingService : LifecycleService() {
     )
 
     private fun startForegroundService() {
+        addEmptyPolyline()
+        isTracking.postValue(true)
+
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
